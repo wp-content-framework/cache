d709897890aa0d4049dfda268f37e2e9817c73bf
Technote <technote.space@gmail.com>
2019-03-24 05:16:57 +0900

    add tests

--- tests/models/cacheTest.php
+++ tests/models/cacheTest.php
@@ -24,7 +24,6 @@ class CacheTest extends \WP_Framework_Cache\Tests\TestCase {
 	 */
 	private static $_option;
 
-
 	public static function setUpBeforeClass() {
 		parent::setUpBeforeClass();
 		static::$_option = \WP_Framework_Cache\Classes\Models\Cache\Option::get_instance( static::$app );
@@ -36,9 +35,10 @@ class CacheTest extends \WP_Framework_Cache\Tests\TestCase {
 	 * @param bool $expected
 	 * @param string $key
 	 * @param string $group
+	 * @param bool $common
 	 */
-	public function test_exists1( $expected, $key, $group ) {
-		$this->assertEquals( $expected, static::$_option->exists( $key, $group ) );
+	public function test_exists1( $expected, $key, $group, $common ) {
+		$this->assertEquals( $expected, static::$_option->exists( $key, $group, $common ) );
 	}
 
 	/**
@@ -46,8 +46,11 @@ class CacheTest extends \WP_Framework_Cache\Tests\TestCase {
 	 */
 	public function _test_exists1_provider() {
 		return [
-			[ false, 'test1', 'default' ],
-			[ false, 'test2', 'test_group' ],
+			[ false, 'test1', 'default', false ],
+			[ false, 'test2', 'test_group', false ],
+
+			[ false, 'test1', 'default', true ],
+			[ false, 'test2', 'test_group', true ],
 		];
 	}
 
@@ -57,10 +60,11 @@ class CacheTest extends \WP_Framework_Cache\Tests\TestCase {
 	 * @param mixed $expected
 	 * @param string $key
 	 * @param string $group
+	 * @param bool $common
 	 * @param mixed $default
 	 */
-	public function test_get1( $expected, $key, $group, $default ) {
-		$this->assertEquals( $expected, static::$_option->get( $key, $group, $default ) );
+	public function test_get1( $expected, $key, $group, $common, $default ) {
+		$this->assertEquals( $expected, static::$_option->get( $key, $group, $common, $default ) );
 	}
 
 	/**
@@ -68,11 +72,17 @@ class CacheTest extends \WP_Framework_Cache\Tests\TestCase {
 	 */
 	public function _test_get1_provider() {
 		return [
-			[ null, 'test1', 'default', null ],
-			[ false, 'test2', 'test_group1', false ],
-			[ true, 'test3', 'test_group3', true ],
-			[ 0, 'test4', 'test_group4', 0 ],
-			[ [], 'test5', 'test_group5', [] ],
+			[ null, 'test1', 'default', false, null ],
+			[ false, 'test2', 'test_group1', false, false ],
+			[ true, 'test3', 'test_group3', false, true ],
+			[ 0, 'test4', 'test_group4', false, 0 ],
+			[ [], 'test5', 'test_group5', false, [] ],
+
+			[ null, 'test1', 'default', true, null ],
+			[ false, 'test2', 'test_group1', true, false ],
+			[ true, 'test3', 'test_group3', true, true ],
+			[ 0, 'test4', 'test_group4', true, 0 ],
+			[ [], 'test5', 'test_group5', true, [] ],
 		];
 	}
 
@@ -83,24 +93,50 @@ class CacheTest extends \WP_Framework_Cache\Tests\TestCase {
 	 * @param string $key
 	 * @param mixed $value
 	 * @param string $group
+	 * @param bool $common
 	 * @param null|int $expire
 	 */
-	public function test_set( $expected, $key, $value, $group, $expire ) {
-		$this->assertEquals( $expected, static::$_option->set( $key, $value, $group, $expire ) );
+	public function test_set( $expected, $key, $value, $group, $common, $expire ) {
+		$this->assertEquals( $expected, static::$_option->set( $key, $value, $group, $common, $expire ) );
 	}
 
 	/**
 	 * @return array
 	 */
 	public function _test_set_provider() {
+		if ( is_multisite() ) {
+			return [
+				[ true, 'test1', 'value1', 'default', false, null ],
+				[ false, 'test1', 'value1', 'default', false, null ],
+				[ true, 'test1', 'value2', 'default', false, null ],
+				[ true, 'test1', 'value1', 'test_group', false, null ],
+				[ false, 'test1', 'value1', 'test_group', false, null ],
+				[ true, 'test2', 'value2', 'default', false, 1 ],
+				[ true, 'test3', 'value3', 'default', false, 3 ],
+
+				[ true, 'test1', 'value1', 'default', true, null ],
+				[ false, 'test1', 'value1', 'default', true, null ],
+				[ true, 'test1', 'value2', 'default', true, null ],
+				[ true, 'test1', 'value1', 'test_group', true, null ],
+				[ false, 'test1', 'value1', 'test_group', true, null ],
+				[ true, 'test2', 'value2', 'default', true, 1 ],
+				[ true, 'test3', 'value3', 'default', true, 3 ],
+			];
+		}
+
 		return [
-			[ true, 'test1', 'value1', 'default', null ],
-			[ false, 'test1', 'value1', 'default', null ],
-			[ true, 'test1', 'value2', 'default', null ],
-			[ true, 'test1', 'value1', 'test_group', null ],
-			[ false, 'test1', 'value1', 'test_group', null ],
-			[ true, 'test2', 'value2', 'default', 1 ],
-			[ true, 'test3', 'value3', 'default', 3 ],
+			[ true, 'test1', 'value1', 'default', false, null ],
+			[ false, 'test1', 'value1', 'default', false, null ],
+			[ true, 'test1', 'value2', 'default', false, null ],
+			[ true, 'test1', 'value1', 'test_group', false, null ],
+			[ false, 'test1', 'value1', 'test_group', false, null ],
+			[ true, 'test2', 'value2', 'default', false, 1 ],
+			[ true, 'test3', 'value3', 'default', false, 3 ],
+
+			[ false, 'test1', 'value2', 'default', true, null ],
+			[ false, 'test1', 'value1', 'test_group', true, null ],
+			[ false, 'test2', 'value2', 'default', true, 1 ],
+			[ false, 'test3', 'value3', 'default', true, 3 ],
 		];
 	}
 
@@ -111,9 +147,10 @@ class CacheTest extends \WP_Framework_Cache\Tests\TestCase {
 	 * @param bool $expected
 	 * @param string $key
 	 * @param string $group
+	 * @param bool $common
 	 */
-	public function test_exists2( $expected, $key, $group ) {
-		$this->assertEquals( $expected, static::$_option->exists( $key, $group ) );
+	public function test_exists2( $expected, $key, $group, $common ) {
+		$this->assertEquals( $expected, static::$_option->exists( $key, $group, $common ) );
 	}
 
 	/**
@@ -121,12 +158,19 @@ class CacheTest extends \WP_Framework_Cache\Tests\TestCase {
 	 */
 	public function _test_exists2_provider() {
 		return [
-			[ true, 'test1', 'default' ],
-			[ true, 'test1', 'test_group' ],
-			[ false, 'test1', 'test_group2' ],
-			[ true, 'test2', 'default' ],
-			[ true, 'test3', 'default' ],
-			[ false, 'test4', 'default' ],
+			[ true, 'test1', 'default', false ],
+			[ true, 'test1', 'test_group', false ],
+			[ false, 'test1', 'test_group2', false ],
+			[ true, 'test2', 'default', false ],
+			[ true, 'test3', 'default', false ],
+			[ false, 'test4', 'default', false ],
+
+			[ true, 'test1', 'default', true ],
+			[ true, 'test1', 'test_group', true ],
+			[ false, 'test1', 'test_group2', true ],
+			[ true, 'test2', 'default', true ],
+			[ true, 'test3', 'default', true ],
+			[ false, 'test4', 'default', true ],
 		];
 	}
 
@@ -137,14 +181,15 @@ class CacheTest extends \WP_Framework_Cache\Tests\TestCase {
 	 * @param mixed $expected
 	 * @param string $key
 	 * @param string $group
+	 * @param bool $common
 	 * @param mixed $default
 	 * @param int $sleep
 	 */
-	public function test_get2( $expected, $key, $group, $default, $sleep = 0 ) {
+	public function test_get2( $expected, $key, $group, $common, $default, $sleep = 0 ) {
 		if ( $sleep > 0 ) {
 			sleep( $sleep );
 		}
-		$this->assertEquals( $expected, static::$_option->get( $key, $group, $default ) );
+		$this->assertEquals( $expected, static::$_option->get( $key, $group, $common, $default ) );
 	}
 
 	/**
@@ -152,12 +197,21 @@ class CacheTest extends \WP_Framework_Cache\Tests\TestCase {
 	 */
 	public function _test_get2_provider() {
 		return [
-			[ 'value2', 'test1', 'default', null ],
-			[ 'value1', 'test1', 'test_group', null ],
-			[ null, 'test1', 'test_group2', null ],
-			[ true, 'test2', 'default', true, 2 ],
-			[ 'value3', 'test3', 'default', true ],
-			[ true, 'test3', 'default', true, 2 ],
+			[ 'value2', 'test1', 'default', false, null ],
+			[ 'value1', 'test1', 'test_group', false, null ],
+			[ null, 'test1', 'test_group2', false, null ],
+			[ true, 'test2', 'default', false, true, 2 ],
+			[ 'value3', 'test3', 'default', false, true ],
+
+			[ 'value3', 'test3', 'default', true, true ],
+
+			[ true, 'test3', 'default', false, true, 2 ],
+
+			[ 'value2', 'test1', 'default', true, null ],
+			[ 'value1', 'test1', 'test_group', true, null ],
+			[ null, 'test1', 'test_group2', true, null ],
+			[ true, 'test2', 'default', true, true, 2 ],
+			[ true, 'test3', 'default', true, true ],
 		];
 	}
 }
