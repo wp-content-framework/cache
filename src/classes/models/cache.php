cd3401447fa985d08e01169fa78c78f84354c80e
Technote <technote.space@gmail.com>
2020-01-01 01:54:10 +0900

    chore: remove version info

--- src/classes/models/cache.php
+++ src/classes/models/cache.php
@@ -2,7 +2,6 @@
 /**
  * WP_Framework_Cache Classes Models Cache
  *
- * @version 0.0.13
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
@@ -28,9 +27,9 @@ class Cache implements \WP_Framework_Core\Interfaces\Loader, \WP_Framework_Cache
 	use Loader, \WP_Framework_Cache\Traits\Cache, Uninstall;
 
 	/**
-	 * @var \WP_Framework_Cache\Interfaces\Cache $_cache
+	 * @var \WP_Framework_Cache\Interfaces\Cache $cache
 	 */
-	private $_cache;
+	private $cache;
 
 	/**
 	 * initialized
@@ -46,13 +45,14 @@ class Cache implements \WP_Framework_Core\Interfaces\Loader, \WP_Framework_Cache
 			}
 		}
 		/** @var Singleton $cache_class */
-		$this->_cache = $cache_class::get_instance( $this->app );
+		$this->cache = $cache_class::get_instance( $this->app );
 	}
 
 	/**
 	 * setup settings
+	 * @noinspection PhpUnusedPrivateMethodInspection
+	 * @SuppressWarnings(PHPMD.UnusedPrivateMethod)
 	 */
-	/** @noinspection PhpUnusedPrivateMethodInspection */
 	private function setup_settings() {
 		if ( ! $this->is_valid_cron_delete() ) {
 			$this->app->setting->remove_setting( 'delete_cache_interval' );
@@ -77,7 +77,7 @@ class Cache implements \WP_Framework_Core\Interfaces\Loader, \WP_Framework_Cache
 	 * @return bool
 	 */
 	public function exists( $key, $group = 'default', $common = false ) {
-		return $this->_cache->exists( $key, $group, $common );
+		return $this->cache->exists( $key, $group, $common );
 	}
 
 	/**
@@ -89,7 +89,7 @@ class Cache implements \WP_Framework_Core\Interfaces\Loader, \WP_Framework_Cache
 	 * @return mixed
 	 */
 	public function get( $key, $group = 'default', $common = false, $default = null ) {
-		return $this->_cache->get( $key, $group, $common, $default );
+		return $this->cache->get( $key, $group, $common, $default );
 	}
 
 	/**
@@ -102,7 +102,7 @@ class Cache implements \WP_Framework_Core\Interfaces\Loader, \WP_Framework_Cache
 	 * @return bool
 	 */
 	public function set( $key, $value, $group = 'default', $common = false, $expire = null ) {
-		return $this->_cache->set( $key, $value, $group, $common, $expire );
+		return $this->cache->set( $key, $value, $group, $common, $expire );
 	}
 
 	/**
